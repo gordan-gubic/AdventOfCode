3ff55c64c66121e429dea2e9f16c73238df68671
Gordan Gubic <gguc@tbdvox.com>
2021-12-09 10:42:15 +0100

    Year 2021. Day 08. Day 09.

---
+++ Gguc.Aoc.Y2021/Days/Day08.cs
@@ -0,0 +1,187 @@
+﻿#define LOGx
+#define STOPWATCH
+
+namespace Gguc.Aoc.Y2021.Days;
+
+public class Day08 : Day
+{
+    private const int YEAR = 2021;
+    private const int DAY = 8;
+
+    private List<string> _source;
+    private List<List<string>> _data;
+    private List<List<int>> _values;
+    private List<List<string>> _sorted;
+
+    public Day08(ILog log, IParser parser) : base(log, parser, YEAR, DAY)
+    {
+        EnableDebug();
+        Initialize();
+    }
+
+    /// <inheritdoc />
+    protected override void InitParser()
+    {
+        Parser.Type = ParserFileType.Real;
+
+        _source = Parser.Parse();
+    }
+
+    /// <inheritdoc />
+    protected override void ProcessData()
+    {
+        _data = new List<List<string>>();
+
+        _source.ForEach(x => _data.Add(x.Split(new char[] { ' ', '|' }, StringSplitOptions.RemoveEmptyEntries).ToList()));
+
+        _values = Decode(_data);
+
+        _sorted = Sorted(_data);
+    }
+
+    private List<List<int>> Decode(List<List<string>> data)
+    {
+        var result = new List<List<int>>();
+
+        foreach (var row in data)
+        {
+            var r = new List<int>();
+
+            foreach (var c in row)
+            {
+                r.Add(c.Length);
+            }
+
+            result.Add(r);
+        }
+
+        return result;
+    }
+
+    private List<List<string>> Sorted(List<List<string>> data)
+    {
+        var result = new List<List<string>>();
+
+        foreach (var row in data)
+        {
+            var r = new List<string>();
+
+            foreach (var text in row)
+            {
+                var x = text.ToList();
+                x.Sort();
+
+                r.Add(x.JoinToString());
+            }
+
+            result.Add(r);
+        }
+
+        return result;
+    }
+
+    protected override void ComputePart1()
+    {
+        Result = 0;
+
+        var hash = new HashSet<int> { 2, 3, 4, 7 };
+
+        foreach (var row in _values)
+        {
+            for (int i = 10; i <= 13; i++)
+            {
+                if (hash.Contains(row[i])) Add(1);
+            }
+        }
+    }
+
+    protected override void ComputePart2()
+    {
+        Result = 0;
+
+        var rowscount = _sorted.Count;
+
+        for (int i = 0; i < rowscount; i++)
+        {
+            var signals = _sorted[i].ToList();
+            var map = CreateMap(signals);
+
+            var sb = new StringBuilder();
+            for (int j = 10; j <= 13; j++)
+            {
+                sb.Append(map[signals[j]]);
+            }
+
+            var output = sb.ToString().ToLong();
+            Add(output);
+        }
+    }
+
+    private Dictionary<string, int> CreateMap(List<string> signals)
+    {
+        var map = new Dictionary<string, int>();
+        var hash = new HashSet<string>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            hash.Add(signals[i]);
+        }
+
+        AddValueToMap(1, hash, map, x => x.Length == 2);
+        AddValueToMap(8, hash, map, x => x.Length == 7);
+
+        var x4 = AddValueToMap(4, hash, map, x => x.Length == 4);
+        var x7 = AddValueToMap(7, hash, map, x => x.Length == 3);
+
+        AddValueToMap(9, hash, map, x => x.Length == 6 && Contains(x, x4) && Contains(x, x7));
+        AddValueToMap(0, hash, map, x => x.Length == 6 && Contains(x, x7));
+
+        var x6 = AddValueToMap(6, hash, map, x => x.Length == 6);
+
+        AddValueToMap(3, hash, map, x => x.Length == 5 && Contains(x, x7));
+        AddValueToMap(5, hash, map, x => x.Length == 5 && Contains(x6, x));
+        AddValueToMap(2, hash, map, x => x.Length == 5);
+
+        return map;
+    }
+
+    private string AddValueToMap(int value, HashSet<string> hash, Dictionary<string, int> map, Func<string, bool> predicate)
+    {
+        var code = hash.Where(predicate).FirstOrDefault();
+        map[code] = value;
+        hash.Remove(code);
+        return code;
+    }
+
+    private bool Contains(string x, string y)
+    {
+        var x1 = x.ToList();
+        var y1 = y.ToList();
+
+        return y1.Except(x1).Count() == 0;
+    }
+
+    #region Dump
+    public override void DumpInput()
+    {
+        DumpData();
+    }
+
+    [Conditional("LOG")]
+    private void DumpData()
+    {
+        if (!Log.EnableDebug) return;
+
+        Debug();
+
+        _data[0].Dump("Item");
+        _data.DumpJson("List");
+
+        _values.DumpJson("Values");
+        _sorted.DumpJson("Sorted");
+    }
+    #endregion Dump
+}
+
+#if DUMP
+#endif
---
+++ Gguc.Aoc.Y2021/Days/Day09.cs
@@ -0,0 +1,181 @@
+﻿#define LOGx
+#define STOPWATCH
+
+namespace Gguc.Aoc.Y2021.Days;
+
+public class Day09 : Day
+{
+    private const int YEAR = 2021;
+    private const int DAY = 9;
+    
+    private List<string> _source;
+    private Map<int> _data;
+    private Map<bool> _basins;
+    private int _width;
+    private int _height;
+
+    public Day09(ILog log, IParser parser) : base(log, parser, YEAR, DAY)
+    {
+        EnableDebug();
+        Initialize();
+    }
+
+    /// <inheritdoc />
+    protected override void InitParser()
+    {
+        Parser.Type = ParserFileType.Real;
+
+        _source = Parser.Parse();
+    }
+
+    /// <inheritdoc />
+    protected override void ProcessData()
+    {
+        _width = _source[0].Length;
+        _height = _source.Count;
+
+        var map = new Map<int>(_width, _height);
+        var basins = new Map<bool>(_width, _height);
+
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                var value = $"{_source[y][x]}".ToInt();
+
+                map.Values[x, y] = value;
+
+                if (value < 9) basins.Values[x, y] = true;
+            }
+        }
+
+        _data = map;
+        _basins = basins;
+    }
+
+    protected override void ComputePart1()
+    {
+        Result = 0;
+        var map = _data;
+
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                ProcessPoint(map, x, y);
+            }
+        }
+    }
+
+    protected override void ComputePart2()
+    {
+        Result = 1;
+        List<int> basinSizes = new List<int>();
+        var basins = _basins.Clone();
+
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                var size = ProcessBasin(basins, x, y);
+
+                if(size > 0) basinSizes.Add(size);
+            }
+        }
+
+        basinSizes.DumpJson();
+
+        FindLargests(basinSizes);
+    }
+
+    private void FindLargests(List<int> basinSizes)
+    {
+        var ordered = basinSizes.OrderByDescending(x => x).ToList();
+
+        Multiply(ordered[0]);
+        Multiply(ordered[1]);
+        Multiply(ordered[2]);
+    }
+
+    private void ProcessPoint(Map<int> map, int x, int y)
+    {
+        var value = map.GetValue(x, y);
+        var up = y - 1;
+        var down = y + 1;
+        var left = x - 1;
+        var right = x + 1;
+
+        if (up >= 0 && map.GetValue(x, up) <= value)
+        {
+            return;
+        }
+        if (down < _height && map.GetValue(x, down) <= value)
+        {
+            return;
+        }
+        if (left >= 0 && map.GetValue(left, y) <= value)
+        {
+            return;
+        }
+        if (right < _width && map.GetValue(right, y) <= value)
+        {
+            return;
+        }
+
+        Add(value + 1);
+    }
+
+    private int ProcessBasin(Map<bool> basins, int x, int y)
+    {
+        var value = basins.GetValue(x, y);
+        if (!value) return 0;
+
+        var total = 1;
+        basins.Values[x, y] = false;
+
+        var up = y - 1;
+        var down = y + 1;
+        var left = x - 1;
+        var right = x + 1;
+
+        if (up >= 0)
+        {
+            total += ProcessBasin(basins, x, up);
+        }
+        if (down < _height)
+        {
+            total += ProcessBasin(basins, x, down);
+        }
+        if (left >= 0)
+        {
+            total += ProcessBasin(basins, left, y);
+        }
+        if (right < _width)
+        {
+            total += ProcessBasin(basins, right, y);
+        }
+
+        return total;
+    }
+
+    #region Dump
+    public override void DumpInput()
+    {
+        DumpData();
+    }
+
+    [Conditional("LOG")]
+    private void DumpData()
+    {
+        if (!Log.EnableDebug) return;
+
+        Debug();
+
+        _data.Dump("Map", true);
+        _basins.Dump("basins", true);
+    }
+    #endregion Dump
+}
+
+#if DUMP
+#endif
