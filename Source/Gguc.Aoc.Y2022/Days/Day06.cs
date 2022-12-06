0e5586c7b12aa4375d492428ed33b816b56c6b6c
TBDVOX\gguc <gguc@tbdvox.com>
2022-12-06 07:13:47 +0100

    Y2022 - Year 2022. Day 06.

---
+++ Source/Gguc.Aoc.Y2022/Days/Day06.cs
@@ -0,0 +1,80 @@
+﻿#define LOGx
+#define STOPWATCH
+
+namespace Gguc.Aoc.Y2022.Days;
+
+public class Day06 : Day
+{
+    private const int YEAR = 2022;
+    private const int DAY = 6;
+
+    private List<string> _data;
+
+    public Day06(ILog log, IParser parser) : base(log, parser, YEAR, DAY)
+    {
+        EnableDebug();
+        Initialize();
+    }
+
+    /// <inheritdoc />
+    protected override void InitParser()
+    {
+        Parser.Type = ParserFileType.Test;
+        Parser.Type = ParserFileType.Real;
+
+        _data = Parser.Parse();
+    }
+
+    /// <inheritdoc />
+    public override void DumpInput()
+    {
+        DumpData();
+    }
+
+    protected override void ComputePart1()
+    {
+        Result = FindMarker(4);
+    }
+
+    protected override void ComputePart2()
+    {
+        Result = FindMarker(14);
+    }
+
+    private long FindMarker(int input)
+    {
+        var line = _data[0];
+
+        for (int i = 0; i < line.Length - input + 1; i++)
+        {
+            var chars = line.Skip(i).Take(input);
+            var areDiff = AreDifferent(chars);
+
+            if (areDiff) return i + input;
+        }
+
+        return 0L;
+    }
+
+    private bool AreDifferent(IEnumerable<char> chars)
+    {
+        var pod = new HashSet<char>(chars);
+        return pod.Count == chars.Count();
+    }
+
+    [Conditional("LOG")]
+    private void DumpData()
+    {
+        if (!Log.EnableDebug) return;
+
+        Debug();
+
+        // _data.DumpCollection();
+
+        Log.Info("Your puzzle answer was 1855.");
+        Log.Info("Your puzzle answer was 3256.");
+    }
+}
+
+#if DUMP
+#endif
