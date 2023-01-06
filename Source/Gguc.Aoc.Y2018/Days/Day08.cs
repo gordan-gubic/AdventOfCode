97f8689c1a3670ffb2e65f8b1db253ab70a361fd
TBDVOX\gguc <gguc@tbdvox.com>
2023-01-06 11:32:58 +0100

    Y2018 - Year 2018. Day 08.

---
+++ Source/Gguc.Aoc.Y2018/Days/Day08.cs
@@ -0,0 +1,124 @@
+﻿#define LOG
+#define STOPWATCH
+
+namespace Gguc.Aoc.Y2018.Days;
+
+public class Day08 : Day
+{
+    private const int YEAR = 2018;
+    private const int DAY = 08;
+
+    private List<string> _data;
+    private List<int> _values;
+    private Dictionary<Guid, Node08> _nodes;
+    private Node08 _root;
+
+    public Day08(ILog log, IParser parser) : base(log, parser, YEAR, DAY)
+    {
+        EnableDebug();
+        Initialize();
+
+        Expected1 = "36027";
+        Expected2 = "23960";
+    }
+
+    protected override void InitParser()
+    {
+        Parser.Type = ParserFileType.Test;
+        Parser.Type = ParserFileType.Real;
+
+        _data = Parser.Parse();
+    }
+
+    #region Parse
+    protected override void ProcessData()
+    {
+        _values = new();
+        _nodes = new();
+
+        var parts = _data[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var p in parts)
+        {
+            _values.Add(p.ToInt());
+        }
+    }
+    #endregion
+
+    #region Head
+    protected override void ComputePart1()
+    {
+        ProcessValues();
+
+        Result = _root.TotalSum;
+    }
+
+    protected override void ComputePart2()
+    {
+        Result = _root.Value;
+    }
+    #endregion
+
+    #region Body
+
+    private void ProcessValues()
+    {
+        var queue = new Queue<int>(_values);
+
+        while (queue.Any())
+        {
+            ProcessRoot(queue);
+        }
+    }
+
+    private void ProcessRoot(Queue<int> queue)
+    {
+        _root = ProcessNextNode(queue);
+    }
+
+    private Node08 ProcessNextNode(Queue<int> queue)
+    {
+        var node = new Node08();
+        var v1 = queue.Dequeue();
+        var v2 = queue.Dequeue();
+
+        for (var i = 0; i < v1; i++)
+        {
+            node.Children.Add(ProcessNextNode(queue));
+        }
+
+        for (var i = 0; i < v2; i++)
+        {
+            node.Values.Add(queue.Dequeue());
+        }
+        
+        node.GetTotalSum();
+        node.GetValue();
+        _nodes[node.Id] = node;
+
+        return node;
+    }
+
+    #endregion
+
+    #region Dump
+    /// <inheritdoc />
+    public override void DumpInput()
+    {
+        DumpData();
+    }
+
+    [Conditional("LOG")]
+    private void DumpData()
+    {
+        if (!Log.EnableDebug) return;
+
+        Debug();
+
+        // _values.DumpCollection("_values");
+    }
+    #endregion
+}
+
+#if DUMP
+#endif
---
+++ Source/Gguc.Aoc.Y2018/Models/Node08.cs
@@ -0,0 +1,62 @@
+﻿namespace Gguc.Aoc.Y2018.Models;
+
+public class Node08
+{
+    public Guid Id { get; } = Guid.NewGuid();
+
+    public Node08 Parent { get; set; }
+
+    public List<Node08> Children { get; set; } = new();
+
+    public List<int> Values { get; set; } = new();
+
+    public long Sum { get; set; }
+
+    public long TotalSum { get; set; }
+
+    public long Value { get; set; } = -1;
+
+    public long GetSum()
+    {
+        if (Sum > 0) return Sum;
+
+        Values.ForEach(x => Sum += x);
+
+        return Sum;
+    }
+
+    public long GetTotalSum()
+    {
+        if (TotalSum > 0) return TotalSum;
+
+        var total = GetSum();
+        Children.ForEach(x => total += x.GetTotalSum());
+
+        TotalSum = total;
+        return TotalSum;
+    }
+
+    public long GetValue()
+    {
+        if (Value >= 0) return Value;
+
+        if (Children.Count == 0)
+        {
+            Value = GetSum();
+            return Value;
+        }
+
+        Value = 0;
+        foreach (var i in Values)
+        {
+            var index = i - 1;
+
+            if (index < Children.Count)
+            {
+                Value += Children[index].GetValue();
+            }
+        }
+
+        return Value;
+    }
+}
